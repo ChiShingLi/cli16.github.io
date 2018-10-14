302869d8eead03307f345b8b299e9bdc125a0473
Chi Li <cli16@mail.wou.edu>
2018-10-13 21:59:50 -0700

    implemented underflow base constructor

---
+++ CS460/hw3/hw3/hw3/hw3/QueueUnderflowException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace hw3
+{
+    class QueueUnderflowException : SystemException
+    {
+        public QueueUnderflowException() : base()
+        { 
+        }
+
+        public QueueUnderflowException(string message) : base(message)
+        {
+        }
+    }
+}
