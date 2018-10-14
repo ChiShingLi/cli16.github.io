4d32a680cdda4595945cd154ea3fc67048f4fb7f
Chi Li <cli16@mail.wou.edu>
2018-10-13 21:57:34 -0700

    added Interface and summary of the code

---
+++ CS460/hw3/hw3/hw3/hw3/QueueInterface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace hw3
+{
+    class QueueInterface
+    {
+        public interface queueInterface<T>
+        {
+            T push(T element);
+            T pop();
+            bool isEmpty();
+        }
+    }
+}
