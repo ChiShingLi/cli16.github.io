d3792dc415eb0c14c3fcd45acd4f091a152f9fc6
Chi Li <cli16@mail.wou.edu>
2018-10-13 21:04:51 -0700

    implemented Node.cs

---
+++ CS460/hw3/hw3/hw3/hw3/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace hw3
+{
+    public class Node<T>
+    {
+        public T data;
+        public Node<T> next;
+
+        public Node(T data, Node<T> next)
+        {
+            this.data = data;
+            this.next = next;
+        }
+    }
+}
